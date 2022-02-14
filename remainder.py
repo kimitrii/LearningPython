f2e98122f9f439c5b1266a332c81fd61400bc211
kimitrii <kimitrimusic@gmail.com>
2022-02-14 13:52:50 -0300

    remainder

---
+++ remainder.py
@@ -0,0 +1,16 @@
+k = 5 % 2
+print(k)
+
+kk = 4 ** 2
+print(kk)
+
+eee = 'hello ' + 'world'
+print(eee)
+
+print(float(99) + 100)
+
+i = 42
+type(i)
+
+wellfcome = input("enter your name: ")
+print("hello",wellfcome)
