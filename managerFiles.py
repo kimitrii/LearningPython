0ec7f9ecfdde769a4ea459f75be6a8a009e9c940
kimitrii <kimitrimusic@gmail.com>
2022-02-18 12:55:18 -0300

    manager files

--- managerFiles.py
+++ managerFiles.py
@@ -11,3 +11,24 @@ stuff = 'hello\nWorld'
 stuff
 'Hello\nWorld!'
 print(stuff)
+
+#read files as a series of characters
+stringsFile = open('Strings.py')
+inp = stringsFile.read()
+print('theres ',len(inp), ' characters')
+
+#searching for something in files
+camera = open('first.py')
+for expecificWorld in camera:
+    #rstrip() clen the empyt lines in return
+    expecificWorld = expecificWorld.strip()
+    if expecificWorld.startswith('n'):
+        print(expecificWorld)
+
+#if file don't exist
+fname = open('adfasdfafs.py')
+try:
+    fname = open(fname)
+except:
+    print('File cannot be opened: ', fname)
+    quit()#quit() makes the code ends without traceback
